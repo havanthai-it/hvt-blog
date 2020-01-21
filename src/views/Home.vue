b90f1d7fbfef0a09806413947950baaa26ffbc4b
havanthai-it <havanthai.it@gmail.com>
2020-01-21 23:37:57 +0700

    My first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1 v-if="3 > 2">3 > 2</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,10 +8,10 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
     HelloWorld
   }
